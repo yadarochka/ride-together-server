85e63328104335bf0280b67d776f88dd4ed854b6
yadarochka <66113821+yadarochka@users.noreply.github.com>
2023-04-12 17:30:59 +0500

    refactor: move server port configuration to .env file

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const express = require("express");
+const cors = require("cors");
 const userRouter = require("./routes/user.routes");
 const rideRouter = require("./routes/ride.routes");
 const ratingRouter = require("./routes/rating.routes");
 
+require("dotenv").config();
+
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use("/api/users", userRouter);
 app.use("/api/ride", rideRouter);
 app.use("/api/rating", ratingRouter);
 
-// запуск сервера на порту 3000
-app.listen(3000, function () {
-  console.log("Приложение запущено на http://localhost:3000");
+const PORT = process.env.APP_PORT;
+app.listen(PORT, function () {
+  console.log(`Приложение запущено PORT = ${PORT}`);
 });
